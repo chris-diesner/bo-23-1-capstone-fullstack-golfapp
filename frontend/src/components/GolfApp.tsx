1f3473b26168bb1ec52d84503c7e1b2a9d46d346
chris-diesner <chr.diesner@mailbox.com>
2023-06-09 13:13:42 +0200

    add Home.tsx & GolfApp.tsx; fixed router links

---
+++ frontend/src/components/GolfApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GolfApp() {
+    return (
+        <div>
+            <h3>Golf App</h3>
+            <br/>
+            <p>work in progress</p>
+        </div>
+    );
+}
+
+export default GolfApp;
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+
+
+
+    return (
+        <div>
+            <h3>Welcome To My GolfApp (WIP)</h3>
+            <br/>
+            <h5>What' new:</h5>
+            <p>Added a Login Page</p>
+            <button onClick={() => window.location.href = "/login"}>Login</button>
+        </div>
+    );
+}
+
+export default Home;

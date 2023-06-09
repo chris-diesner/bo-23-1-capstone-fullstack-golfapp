06c291837272b32799e74fc21ea8e1a4e24a01be
chris-diesner <chr.diesner@mailbox.com>
2023-06-09 11:44:05 +0200

    install router, useNavigate & add Login.tsx in frontend

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,40 @@
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+type Props = {
+    login: (username: string, password: string) => Promise<void>;
+}
+
+function Login(props:Props) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
+
+    function loginOnSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        props.login(username, password).then(() => {
+            navigate("/");
+        });
+    }
+
+    function onChangeHandlerUsername(e: ChangeEvent<HTMLInputElement>) {
+        setUsername(e.currentTarget.value);
+    }
+
+    function onChangeHandlerPassword(e: ChangeEvent<HTMLInputElement>) {
+        setPassword(e.currentTarget.value);
+    }
+
+    return (
+        <div>
+            <h3>Login</h3>
+            <form onSubmit={loginOnSubmit}>
+                <input type="username" onChange={onChangeHandlerUsername}/>
+                <input type="password" onChange={onChangeHandlerPassword}/>
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;

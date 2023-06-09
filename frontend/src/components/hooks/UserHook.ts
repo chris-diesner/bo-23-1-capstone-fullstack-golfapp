389e13dc872b6c9b9e783d01f0d13aa4570cdd17
chris-diesner <chr.diesner@mailbox.com>
2023-06-09 11:09:04 +0200

    install axios & add UserHook in frontend

---
+++ frontend/src/components/hooks/UserHook.ts
@@ -0,0 +1,11 @@
+import {useState} from "react";
+import axios from "axios";
+
+export default function UserHook () {
+    const [user, setUser] = useState<String>();
+    function login(username: string, password: string) {
+        return axios.post("/user/login", undefined, {auth: {username, password}})
+            .then((response) => setUser(response.data))
+    }
+    return {login, user}
+}

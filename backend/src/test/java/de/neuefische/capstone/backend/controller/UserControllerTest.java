19bcd7b5d84f7d4f932a2c6ba04883f28a6a064f
chris-diesner <chr.diesner@mailbox.com>
2023-06-12 10:16:33 +0200

    add integration test for UserController.java

--- backend/src/test/java/de/neuefische/capstone/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/neuefische/capstone/backend/controller/UserControllerTest.java
@@ -1,15 +1,42 @@
 package de.neuefische.capstone.backend.controller;
 
+import de.neuefische.capstone.backend.model.GolfUser;
+import de.neuefische.capstone.backend.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest
-class UserControllerTest {
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void registerUser() {
+    @DirtiesContext
+    @WithMockUser
+    public void registerUser_shouldReturnIsCreated_andShouldReturnUser() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/user/register")
+                        .param("username", "test")
+                        .param("password", "test")
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("test"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").isNotEmpty());
     }
 }

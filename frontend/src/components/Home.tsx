920fe444b0102fa2cc7783362dc7288adb7261e3
chris-diesner <chr.diesner@mailbox.com>
2023-06-12 21:10:10 +0200

    add css for Home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
+        <div className="Home">
             <h3>Welcome To My GolfApp (WIP)</h3>
             <br/>
             <h5>What' new:</h5>

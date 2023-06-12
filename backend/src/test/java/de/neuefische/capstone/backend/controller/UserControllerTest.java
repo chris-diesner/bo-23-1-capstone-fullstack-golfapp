10f516f04dd12303ae747f8a0e1dabc3f76c0b0c
chris-diesner <chr.diesner@mailbox.com>
2023-06-12 10:17:33 +0200

    add integration test for UserController.java (clean code)

--- backend/src/test/java/de/neuefische/capstone/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/neuefische/capstone/backend/controller/UserControllerTest.java
@@ -1,21 +1,14 @@
 package de.neuefische.capstone.backend.controller;
 
-import de.neuefische.capstone.backend.model.GolfUser;
-import de.neuefische.capstone.backend.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest

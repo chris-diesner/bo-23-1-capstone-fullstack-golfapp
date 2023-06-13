923108b14c5cd1c57b9ae701c779ba006721c81a
chris-diesner <chr.diesner@mailbox.com>
2023-06-13 15:57:34 +0200

    added StatusController.java

---
+++ backend/src/main/java/de/neuefische/capstone/backend/controller/StatusController.java
@@ -0,0 +1,15 @@
+package de.neuefische.capstone.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/status")
+public class StatusController {
+
+    @GetMapping
+    public String getStatus() {
+        return "Backend is running";
+    }
+}

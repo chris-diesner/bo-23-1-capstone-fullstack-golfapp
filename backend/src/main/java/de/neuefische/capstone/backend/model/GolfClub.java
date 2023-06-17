2502250c52deea0fb118258f4d8017cdcb91eccb
chris-diesner <chr.diesner@mailbox.com>
2023-06-17 09:46:33 +0200

    added model for GolfClub & GolfCourse

---
+++ backend/src/main/java/de/neuefische/capstone/backend/model/GolfClub.java
@@ -0,0 +1,28 @@
+package de.neuefische.capstone.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("Clubs")
+public class GolfClub {
+    private String clubID;
+    private String clubName;
+    private String city;
+    private String state;
+    private String country;
+    private String address;
+    private String postalCode;
+    private String latitude;
+    private String longitude;
+    private String website;
+    private String telephone;
+    private String timestampUpdated;
+    private List<GolfCourse> courses;
+}

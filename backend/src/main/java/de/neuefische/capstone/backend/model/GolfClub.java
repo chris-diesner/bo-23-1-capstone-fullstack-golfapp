7fdcf72fe832b1b4f2ac80847a74918b994bf46c
chris-diesner <chr.diesner@mailbox.com>
2023-06-17 12:11:50 +0200

    added test for golf clubs

--- backend/src/main/java/de/neuefische/capstone/backend/model/GolfClub.java
+++ backend/src/main/java/de/neuefische/capstone/backend/model/GolfClub.java
@@ -1,6 +1,7 @@
 package de.neuefische.capstone.backend.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Document("Clubs")
 public class GolfClub {
     @Id
---
+++ backend/src/test/java/de/neuefische/capstone/backend/controller/GolfClubControllerTest.java
@@ -0,0 +1,47 @@
+package de.neuefische.capstone.backend.controller;
+
+import de.neuefische.capstone.backend.model.GolfClub;
+import de.neuefische.capstone.backend.service.GolfClubService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GolfClubControllerTest {
+
+    @Mock
+    private GolfClubService golfClubService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getGolfClubByClubID_shouldReturn200OK_shouldReturnEmptyList() throws Exception {
+    }
+
+    @Test
+    void getAllGolfClubs_shouldReturn200OK_shouldReturnEmptyList() throws Exception {
+        mockMvc.perform(get("/api/golfapp/clubs"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[]"));
+    }
+}

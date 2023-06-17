bc0ca44884a76cca60cde77cfbe2b40ce8367f6b
chris-diesner <chr.diesner@mailbox.com>
2023-06-17 10:24:14 +0200

    added service for golf club (surely incomplete)

--- backend/src/main/java/de/neuefische/capstone/backend/repo/GolfClubRepo.java
+++ backend/src/main/java/de/neuefische/capstone/backend/repo/GolfClubRepo.java
@@ -2,9 +2,11 @@ package de.neuefische.capstone.backend.repo;
 
 import de.neuefische.capstone.backend.model.GolfClub;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GolfClubRepo extends MongoRepository<GolfClub, String> {
     public Optional<GolfClub> findGolfClubByClubID(String clubID);
 }
---
+++ backend/src/main/java/de/neuefische/capstone/backend/service/GolfClubService.java
@@ -0,0 +1,24 @@
+package de.neuefische.capstone.backend.service;
+
+import de.neuefische.capstone.backend.model.GolfClub;
+import de.neuefische.capstone.backend.repo.GolfClubRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GolfClubService {
+
+    private final GolfClubRepo golfClubRepo;
+
+    public GolfClub getGolfClubByClubID(String clubID) {
+        return golfClubRepo.findGolfClubByClubID(clubID).orElseThrow();
+    }
+
+    public List<GolfClub> getAllGolfClubs() {
+        return golfClubRepo.findAll();
+    }
+}

a0de738fdb7e318b7bbcbfdb360cc18944d5b3cc
chris-diesner <chr.diesner@mailbox.com>
2023-06-18 10:35:37 +0200

    added GolfAppHook.ts

---
+++ frontend/src/components/hooks/GolfAppHook.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import {useState} from "react";
+import {GolfClub} from "../../models/GolfClub";
+
+export default function GolfClubHook() {
+    const [golfClub, setGolfClub] = useState<GolfClub | null>(null);
+
+    function getGolfClub(clubID: string): Promise<void> {
+        return axios
+            .get("/api/golfapp/clubs")
+            .then((response) => {
+                const golfClub: GolfClub = {
+                    clubID: response.data.clubID,
+                    clubName: response.data.clubName,
+                    city: response.data.city,
+                    state: response.data.state,
+                    country: response.data.country,
+                    address: response.data.address,
+                    postalCode: response.data.postalCode,
+                    latitude: response.data.latitude,
+                    longitude: response.data.longitude,
+                    website: response.data.website,
+                    telefone: response.data.telefone,
+                    timestampUpdated: response.data.timestampUpdated,
+                    courses: response.data.courses,
+                };
+                setGolfClub(golfClub);
+            })
+            .catch((error) => console.log(error));
+    }
+
+    return {
+        golfClub,
+        getGolfClub
+    };
+}

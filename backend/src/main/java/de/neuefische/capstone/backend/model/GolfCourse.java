d34f1638c778dafbf3a4a1b336b61be71c18b217
chris-diesner <chr.diesner@mailbox.com>
2023-06-19 07:47:14 +0200

    rework on GolfCourse models & added gjon library to handle json (not comletly testet)

--- backend/src/main/java/de/neuefische/capstone/backend/model/GolfCourse.java
+++ backend/src/main/java/de/neuefische/capstone/backend/model/GolfCourse.java
@@ -6,73 +6,97 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import jakarta.validation.Valid;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Document("Courses")
 public class GolfCourse {
-    private String apiRequestsLeft;
-    private String clubID;
-    private String clubName;
-    private String address;
-    private String postalCode;
-    private String city;
-    private String state;
-    private String country;
-    private String latitude;
-    private String longitude;
-    private String website;
-    private String telephone;
-    private String courseID;
-    private String courseName;
-    private String numHoles;
-    private String timestampUpdated;
-    private String hasGPS;
-    private String measure;
-    private List<Integer> parsMen;
-    private List<Integer> indexesMen;
-    private List<Integer> parsWomen;
-    private List<Integer> indexesWomen;
-    private int numTees;
-    private List<GolfTee> tees;
+
+    @SerializedName("apiRequestsLeft")
+    @Expose
+    public String apiRequestsLeft;
+    @SerializedName("clubID")
+    @Expose
+    public String clubID;
+    @SerializedName("clubName")
+    @Expose
+    public String clubName;
+    @SerializedName("address")
+    @Expose
+    public String address;
+    @SerializedName("postalCode")
+    @Expose
+    public String postalCode;
+    @SerializedName("city")
+    @Expose
+    public String city;
+    @SerializedName("state")
+    @Expose
+    public String state;
+    @SerializedName("country")
+    @Expose
+    public String country;
+    @SerializedName("latitude")
+    @Expose
+    public String latitude;
+    @SerializedName("longitude")
+    @Expose
+    public String longitude;
+    @SerializedName("website")
+    @Expose
+    public String website;
+    @SerializedName("telephone")
+    @Expose
+    public String telephone;
+    @SerializedName("courseID")
+    @Expose
+    public String courseID;
+    @SerializedName("courseName")
+    @Expose
+    public String courseName;
+    @SerializedName("numHoles")
+    @Expose
+    public String numHoles;
+    @SerializedName("timestampUpdated")
+    @Expose
+    public String timestampUpdated;
+    @SerializedName("hasGPS")
+    @Expose
+    public String hasGPS;
+    @SerializedName("measure")
+    @Expose
+    public String measure;
+    @SerializedName("parsMen")
+    @Expose
+    @Valid
+    public List<Integer> parsMen;
+    @SerializedName("indexesMen")
+    @Expose
+    @Valid
+    public List<Integer> indexesMen;
+    @SerializedName("parsWomen")
+    @Expose
+    @Valid
+    public List<Integer> parsWomen;
+    @SerializedName("indexesWomen")
+    @Expose
+    @Valid
+    public List<Integer> indexesWomen;
+    @SerializedName("numTees")
+    @Expose
+    public Integer numTees;
+    @SerializedName("tees")
+    @Expose
+    @Valid
+    public List<Tee> tees;
+    @SerializedName("numCoordinates")
+    @Expose
+    public Integer numCoordinates;
+
 }
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class GolfTee {
-    private String teeID;
-    private String teeName;
-    private String teeColor;
-    private int length1;
-    private int length2;
-    private int length3;
-    private int length4;
-    private int length5;
-    private int length6;
-    private int length7;
-    private int length8;
-    private int length9;
-    private int length10;
-    private int length11;
-    private int length12;
-    private int length13;
-    private int length14;
-    private int length15;
-    private int length16;
-    private int length17;
-    private int length18;
-    private double courseRatingMen;
-    private int slopeMen;
-    private double courseRatingWomen;
-    private int slopeWomen;
-    private double courseRatingMenFront9;
-    private double courseRatingMenBack9;
-    private int slopeMenFront9;
-    private int slopeMenBack9;
-    private double courseRatingWomenFront9;
-    private double courseRatingWomenBack9;
-    private int slopeWomenFront9;
-    private int slopeWomenBack9;
-}
---
+++ backend/src/main/java/de/neuefische/capstone/backend/model/Tee.java
@@ -0,0 +1,114 @@
+package de.neuefische.capstone.backend.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tee {
+
+    @SerializedName("teeID")
+    @Expose
+    public String teeID;
+    @SerializedName("teeName")
+    @Expose
+    public String teeName;
+    @SerializedName("teeColor")
+    @Expose
+    public String teeColor;
+    @SerializedName("length1")
+    @Expose
+    public Integer length1;
+    @SerializedName("length2")
+    @Expose
+    public Integer length2;
+    @SerializedName("length3")
+    @Expose
+    public Integer length3;
+    @SerializedName("length4")
+    @Expose
+    public Integer length4;
+    @SerializedName("length5")
+    @Expose
+    public Integer length5;
+    @SerializedName("length6")
+    @Expose
+    public Integer length6;
+    @SerializedName("length7")
+    @Expose
+    public Integer length7;
+    @SerializedName("length8")
+    @Expose
+    public Integer length8;
+    @SerializedName("length9")
+    @Expose
+    public Integer length9;
+    @SerializedName("length10")
+    @Expose
+    public Integer length10;
+    @SerializedName("length11")
+    @Expose
+    public Integer length11;
+    @SerializedName("length12")
+    @Expose
+    public Integer length12;
+    @SerializedName("length13")
+    @Expose
+    public Integer length13;
+    @SerializedName("length14")
+    @Expose
+    public Integer length14;
+    @SerializedName("length15")
+    @Expose
+    public Integer length15;
+    @SerializedName("length16")
+    @Expose
+    public Integer length16;
+    @SerializedName("length17")
+    @Expose
+    public Integer length17;
+    @SerializedName("length18")
+    @Expose
+    public Integer length18;
+    @SerializedName("courseRatingMen")
+    @Expose
+    public Double courseRatingMen;
+    @SerializedName("slopeMen")
+    @Expose
+    public Double slopeMen;
+    @SerializedName("courseRatingWomen")
+    @Expose
+    public Double courseRatingWomen;
+    @SerializedName("slopeWomen")
+    @Expose
+    public Double slopeWomen;
+    @SerializedName("courseRatingMenFront9")
+    @Expose
+    public Double courseRatingMenFront9;
+    @SerializedName("courseRatingMenBack9")
+    @Expose
+    public Double courseRatingMenBack9;
+    @SerializedName("slopeMenFront9")
+    @Expose
+    public Double slopeMenFront9;
+    @SerializedName("slopeMenBack9")
+    @Expose
+    public Double slopeMenBack9;
+    @SerializedName("courseRatingWomenFront9")
+    @Expose
+    public Double courseRatingWomenFront9;
+    @SerializedName("courseRatingWomenBack9")
+    @Expose
+    public Double courseRatingWomenBack9;
+    @SerializedName("slopeWomenFront9")
+    @Expose
+    public Double slopeWomenFront9;
+    @SerializedName("slopeWomenBack9")
+    @Expose
+    public Double slopeWomenBack9;
+
+}

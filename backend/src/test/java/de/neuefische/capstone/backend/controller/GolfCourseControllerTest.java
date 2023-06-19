1fe6a4645eab473cc2e75d79050fe410f48269cb
chris-diesner <chr.diesner@mailbox.com>
2023-06-19 13:22:36 +0200

    added integration test for GolfClubController

---
+++ backend/src/test/java/de/neuefische/capstone/backend/controller/GolfCourseControllerTest.java
@@ -0,0 +1,45 @@
+package de.neuefische.capstone.backend.controller;
+
+import de.neuefische.capstone.backend.model.GolfCourse;
+import de.neuefische.capstone.backend.service.GolfCourseService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GolfCourseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GolfCourseService golfCourseService;
+
+    @Test
+    public void testGetGolfCourseByCourseID() throws Exception {
+        // Mock GolfCourse data
+        GolfCourse mockGolfCourse = new GolfCourse();
+        mockGolfCourse.setCourseID("123");
+        mockGolfCourse.setCourseName("Test Course");
+        // Mock the service method
+        when(golfCourseService.getGolfCourseByCourseID("123")).thenReturn(mockGolfCourse);
+
+        // Perform GET request
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/golfapp/course/123")
+                        .contentType(MediaType.APPLICATION_JSON))
+                // Assert response
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.courseID").value("123"))
+                .andExpect(jsonPath("$.courseName").value("Test Course"));
+    }
+
+}

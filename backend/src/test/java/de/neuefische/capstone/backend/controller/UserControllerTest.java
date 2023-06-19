066c9c1acb7170ab7c68a1a9056665604d39acad
chris-diesner <chr.diesner@mailbox.com>
2023-06-19 10:16:26 +0200

    remove webenvironment for integration test cause it's not needed annymore

--- backend/src/test/java/de/neuefische/capstone/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/neuefische/capstone/backend/controller/UserControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
 

94dc298d3a3c2c9c5c84ef691cc09d4fef45c6c2
chris-diesner <chr.diesner@mailbox.com>
2023-06-20 11:06:53 +0200

    remove unneeded imports in course controller test

--- backend/src/main/java/de/neuefische/capstone/backend/controller/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/capstone/backend/controller/ControllerAdvisor.java
@@ -5,8 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.NoSuchElementException;
-
 @ControllerAdvice
 public class ControllerAdvisor {
 

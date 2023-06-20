3de8f77eebcdc4178b9a465a205398c85fc4aa4f
chris-diesner <chr.diesner@mailbox.com>
2023-06-20 11:06:41 +0200

    remove unneeded imports in course controller test

--- backend/src/main/java/de/neuefische/capstone/backend/controller/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/capstone/backend/controller/ControllerAdvisor.java
@@ -14,9 +14,4 @@ public class ControllerAdvisor {
     public ResponseEntity<Object> handleInputMismatchException(IllegalArgumentException illegalArgumentException) {
         return new ResponseEntity<>(illegalArgumentException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Object> handleInputMismatchException(NoSuchElementException noSuchElementException) {
-        return new ResponseEntity<>(noSuchElementException.getMessage(), HttpStatus.NOT_FOUND);
-    }
 }

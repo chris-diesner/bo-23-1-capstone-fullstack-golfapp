50a14c216bfafb815944ae322edbc3ff8071407c
chris-diesner <chr.diesner@mailbox.com>
2023-06-24 11:13:47 +0200

    removed unused variable

--- frontend/src/components/golfappMain/Scorecard.tsx
+++ frontend/src/components/golfappMain/Scorecard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import HoleCard from "./HoleCard";

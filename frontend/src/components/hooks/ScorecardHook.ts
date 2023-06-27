e19dfb34931026a05110ad1f537e77ca209b2266
chris-diesner <chr.diesner@mailbox.com>
2023-06-27 17:47:59 +0200

    adjust ScorecardHook.ts for new features

--- frontend/src/components/hooks/ScorecardHook.ts
+++ frontend/src/components/hooks/ScorecardHook.ts
@@ -49,7 +49,57 @@ const useScorecardHook = () => {
         }
     }
 
-    return { loading, error, saveScorecard, editScorecard };
-};
+    const getScorecardsByUserId = async (userId: string) => {
+        try {
+            setLoading(true);
+            setError('');
+            const response = await axios.get('/api/golfapp/scorecard/user/' + userId);
+            const scorecards = response.data;
+            console.log('Scorecards fetched successfully.');
+            return scorecards;
+        } catch (error) {
+            console.error('Error:', error);
+            setError('Something went wrong.');
+            throw error;
+        } finally {
+            setLoading(false);
+        }
+    }
 
+    const getScorecardById = async (scorecardId: string) => {
+        try {
+            setLoading(true);
+            setError('');
+            const response = await axios.get('/api/golfapp/scorecard/' + scorecardId);
+            const scorecard = response.data;
+            console.log('Scorecard fetched successfully.');
+            return scorecard;
+        } catch (error) {
+            console.error('Error:', error);
+            setError('Something went wrong.');
+            throw error;
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const deleteScorecard = async (scorecardId: string) => {
+        try {
+            setLoading(true);
+            setError('');
+            const response = await axios.delete('/api/golfapp/scorecard/' + scorecardId);
+            const scorecard = response.data;
+            console.log('Scorecard deleted successfully.');
+            return scorecard;
+        } catch (error) {
+            console.error('Error:', error);
+            setError('Something went wrong.');
+            throw error;
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return {loading, error, saveScorecard, editScorecard, getScorecardById, getScorecardsByUserId, deleteScorecard}
+}
 export default useScorecardHook;

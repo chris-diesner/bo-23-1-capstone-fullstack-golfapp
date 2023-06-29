978fc88b46090d55726e973df15e2d7cdf10827c
chris-diesner <chr.diesner@mailbox.com>
2023-06-29 14:02:12 +0200

    added style for basic statistic page (WIP)

--- frontend/src/components/golfappMain/UserStatistics.tsx
+++ frontend/src/components/golfappMain/UserStatistics.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Scorecard, Score } from '../../models/Scorecard';
+import React, {useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {Scorecard, Score} from '../../models/Scorecard';
 import useScorecardHook from '../hooks/ScorecardHook';
-import { setScorecard } from '../../Actions/GolfAppActions';
-import userDetails from './UserDetails';
 import UserHook from '../hooks/UserHook';
+import {Container} from "react-bootstrap";
+import '../../styles/AppStyle.css';
 
 type Props = {
     logout: () => Promise<void>;
 };
 
 function UserStatistics(props: Props) {
-    const { userDetails, getUserDetails, user } = UserHook();
+    const {userDetails, getUserDetails, user} = UserHook();
     const navigate = useNavigate();
     const [scorecards, setScorecards] = useState<Scorecard[]>([]);
-    const { loading, error, getScorecardsByUserId } = useScorecardHook();
+    const {loading, error, getScorecardsByUserId} = useScorecardHook();
 
     useEffect(() => {
         getUserDetails()
@@ -38,7 +38,6 @@ function UserStatistics(props: Props) {
             });
     }, [userDetails?.id, user]);
 
-    // Berechnung der Statistiken
     const calculateAveragePutts = () => {
         if (scorecards.length === 0) return 0;
 
@@ -75,18 +74,60 @@ function UserStatistics(props: Props) {
         return fairwayHitPercentage.toFixed(2);
     };
 
+    function onClickLogout() {
+        props.logout()
+            .then(() => {
+                navigate("/")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
     return (
-        <div>
-            <h1>User Statistics</h1>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: </p>}
-            {!loading && !error && (
-                <>
-                    <p>Average Putts: {calculateAveragePutts()}</p>
-                    <p>Fairway Hit Percentage: {calculateFairwayHitPercentage()}%</p>
-                    {/* Weitere Statistiken hier anzeigen */}
-                </>
-            )}
+        <div className="AppContainer">
+            <Container className="d-flex flex-column justify-content-center">
+                <div className="AppContent">
+                    <div className="AppHeader">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                             className="bi bi-x-circle" viewBox="0 0 16 16" onClick={() => window.history.back()}>
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                            <path
+                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                        <h6>Your Statistics</h6>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                             className="bi bi-box-arrow-right" viewBox="0 0 16 16" onClick={onClickLogout}>
+                            <path
+                                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                            <path
+                                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                        </svg>
+                    </div>
+                    <br/>
+                    <div className="AppList">
+                        {loading && <div>Loading...</div>}
+                        {!loading && !error && (
+                            <>
+                                <div className="AppBody">
+                                    <div className="AppHeader">
+                                        <div>
+                                            Average Putts: {calculateAveragePutts()}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="AppBody">
+                                    <div className="AppHeader">
+                                        <div>
+                                            Fairway Hit Percentage: {calculateFairwayHitPercentage()}%
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 }

e39e5c5ebe7fd9f686654f26d64c54586fd9803e
chris-diesner <chr.diesner@mailbox.com>
2023-07-07 19:15:35 +0200

    fitBounds to Markers for every hole

--- frontend/src/components/golfappMain/CourseMap.tsx
+++ frontend/src/components/golfappMain/CourseMap.tsx
@@ -56,6 +56,15 @@ export default function CourseMap(props: Props) {
         return R * c;
     };
 
+    const fitBoundsToMarkers = () => {
+        if (mapRef.current && marker1Ref.current && marker2Ref.current) {
+            const bounds = new google.maps.LatLngBounds();
+            bounds.extend(marker1Ref.current.getPosition()!);
+            bounds.extend(marker2Ref.current.getPosition()!);
+            mapRef.current.fitBounds(bounds);
+        }
+    };
+
     const getCoordinatesForHole = (holeNumber: number): Coordinates | null => {
         const filteredCoordinates = courseCoordinates?.coordinates.filter(
             (coordinate: Coordinates) => coordinate.poi === "1" && coordinate.location === "2"
@@ -85,16 +94,19 @@ export default function CourseMap(props: Props) {
             }
 
             const marker1 = new google.maps.Marker({
-                position: { lat: coordinatesForHole.latitude, lng: coordinatesForHole.longitude },
+                position: {
+                    lat: coordinatesForHole.latitude,
+                    lng: coordinatesForHole.longitude,
+                },
                 map: mapRef.current,
-                icon: Tee
+                icon: Tee,
             });
 
             marker1Ref.current = marker1;
 
             const path: LatLngLiteral[] = [
                 marker1.getPosition()!.toJSON(),
-                currentPosition || { lat: 0, lng: 0 }
+                currentPosition || { lat: 0, lng: 0 },
             ];
 
             setPolylinePath(path);
@@ -104,20 +116,35 @@ export default function CourseMap(props: Props) {
                 geodesic: true,
                 strokeColor: "#669DF6",
                 strokeOpacity: 1.0,
-                strokeWeight: 2
+                strokeWeight: 2,
             });
 
-            polylineRef.current = polyline; // Save reference to polyline
+            polylineRef.current = polyline;
 
             if (mapRef.current) {
                 polyline.setMap(mapRef.current);
             }
+
+            const marker2 = new google.maps.Marker({
+                position: currentPosition || { lat: 0, lng: 0 },
+                map: mapRef.current,
+                icon: Tee,
+            });
+
+            marker2Ref.current = marker2;
+
+            fitBoundsToMarkers();
         }
     }, [props.holeNumber, currentPosition]);
+
 
     useEffect(() => {
         updateMap();
     }, [updateMap])
+
+    useEffect(() => {
+        fitBoundsToMarkers();
+    }, [props.holeNumber]);
 
     useEffect(() => {
         const watchId = navigator.geolocation.watchPosition(
@@ -144,9 +171,21 @@ export default function CourseMap(props: Props) {
     const onLoad = useCallback((map: CourseMap) => {
         mapRef.current = map;
 
-        const bounds = new window.google.maps.LatLngBounds();
-        bounds.extend({ lat: 52.7859645, lng: 13.5724521 });
-        bounds.extend({ lat: 52.78876430772261, lng: 13.570415971724204 });
+        const bounds = new google.maps.LatLngBounds();
+
+        if (marker1Ref.current) {
+            bounds.extend(marker1Ref.current.getPosition()!);
+        }
+
+        if (marker2Ref.current) {
+            bounds.extend(marker2Ref.current.getPosition()!);
+        }
+
+        if (bounds.isEmpty()) {
+            bounds.extend({ lat: 52.7859645, lng: 13.5724521 });
+            bounds.extend({ lat: 52.78876430772261, lng: 13.570415971724204 });
+        }
+
         map.fitBounds(bounds);
 
         const zoomChangeBoundsListener = google.maps.event.addListenerOnce(

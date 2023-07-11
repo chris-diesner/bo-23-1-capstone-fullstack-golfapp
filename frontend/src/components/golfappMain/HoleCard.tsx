23b568fd9d65a60339cdb03779a4e2911dd29b7b
Christian Diesner <130060741+chris-diesner@users.noreply.github.com>
2023-07-11 16:42:54 +0200

    rework on scorecard map

--- frontend/src/components/golfappMain/HoleCard.tsx
+++ frontend/src/components/golfappMain/HoleCard.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Container} from "react-bootstrap";
-import { Score } from "../../models/Scorecard";
-import { useSelector, useDispatch } from "react-redux";
-import { setScorecard } from "../../Actions/GolfAppActions";
+import {Score} from "../../models/Scorecard";
+import {useSelector, useDispatch} from "react-redux";
+import {setScorecard} from "../../Actions/GolfAppActions";
 import MapContainer from "./MapContainer";
 import {useNavigate} from "react-router-dom";
+import '../../styles/App.css'
+import ScorecardHook from "../hooks/ScorecardHook";
 
 type Props = {
     score: Score;
@@ -17,21 +19,22 @@ type Props = {
 };
 
 function HoleCard(props: Props) {
-    const { score, holeNumber, onNextHole, onPrevHole, hasNextHole, hasPrevHole, isLastHole } = props;
-    const [holeScore, setHoleScore] = useState<Score>({ ...score });
+    const {score, holeNumber, onNextHole, onPrevHole, hasNextHole, hasPrevHole, isLastHole} = props;
+    const [holeScore, setHoleScore] = useState<Score>({...score});
     const golfCourse = useSelector((state: any) => state.golfApp.selectedGolfCourse)
     const selectedTee = useSelector((state: any) => state.golfApp.golfTee);
     const scorecard = useSelector((state: any) => state.golfApp.scorecard);
     const courseCoordinates = useSelector((state: any) => state.golfApp.courseCoordinates);
     const dispatch = useDispatch();
     const navigate = useNavigate()
+    const { deleteScorecard } = ScorecardHook();
 
     useEffect(() => {
-        setHoleScore({ ...score });
+        setHoleScore({...score});
     }, [score]);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value, type, checked } = event.target;
+        const {name, value, type, checked} = event.target;
         const inputValue = type === 'checkbox' ? checked : value;
 
         setHoleScore((prevScore) => ({
@@ -107,22 +110,54 @@ function HoleCard(props: Props) {
         navigate("/golfapp/finalscorecard");
     }
 
+    function handlerOnClickAbortRound(scorecardID: string) {
+        deleteScorecard(scorecardID).then(() =>
+            navigate("/golfapp/")
+        );
+    }
+
     return (
         <div className="HoleContainer">
             <Container className="d-flex flex-column justify-content-center">
-                <div className="GolfSelectPlayerContent">
-                    {courseCoordinates && (
-                        <MapContainer showClubMap={false} golfClubs={[]} holeNumber={holeNumber} />
-                    )}
-                    <div className="HoleHeader">
-                        <h6>
-                            Hole {holeNumber}, {selectedTee?.[`length${holeNumber}`]} {golfCourse.measure}, Par:{" "}
-                            {par}, HCP: {golfCourse.indexesMen[props.holeNumber - 1]},
-                            Personal Par: {holeScore?.personalPar + par}
-                        </h6>
+                <div className="AppContent">
+                    <div className="MapContainer" style={{position: "relative"}}>
+                        {courseCoordinates && (
+                            <MapContainer showClubMap={false} golfClubs={[]} holeNumber={holeNumber}/>
+                        )}
+                        <div className="HoleHeader" style={{position: "absolute", top: 0}}>
+                            <div className="CourseInfo">
+                                <span>Hole {holeNumber}</span>
+                            </div>
+                            <div className="CourseInfo">
+                                {selectedTee?.[`length${holeNumber}`]} {golfCourse.measure}
+                            </div>
+                            <div className="CourseInfo">
+                                Par:{" "}
+                                {par}
+                            </div>
+                            <div className="CourseInfo">
+                                C-HCP: {golfCourse.indexesMen[props.holeNumber - 1]}
+                            </div>
+                            <div className="CourseInfo">
+                                HCP Par: {holeScore?.personalPar + par}
+                            </div>
+                        </div>
                     </div>
                     <br/>
                     <div className="HoleForm">
+                        <div className="ScorecardHeader">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                 className="bi bi-x-circle" viewBox="0 0 16 16"
+                                 onClick={() => handlerOnClickAbortRound(scorecard.scorecardId)}>
+                                <path
+                                    d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path
+                                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                            <h6>Scorecard</h6>
+                            <div></div>
+                        </div>
+                        <br/>
                         <div className="form-group">
                             <label htmlFor={`totalStrokes_${holeNumber}`}>Total Strokes:</label>
                             <input

348b3013725d62c4db903358ffad0c4e02725e92
Christian Diesner <130060741+chris-diesner@users.noreply.github.com>
2023-07-11 16:55:29 +0200

    rework on scorecard map #2

--- frontend/src/components/golfappMain/HoleCard.tsx
+++ frontend/src/components/golfappMain/HoleCard.tsx
@@ -27,7 +27,7 @@ function HoleCard(props: Props) {
     const courseCoordinates = useSelector((state: any) => state.golfApp.courseCoordinates);
     const dispatch = useDispatch();
     const navigate = useNavigate()
-    const { deleteScorecard } = ScorecardHook();
+    const {deleteScorecard} = ScorecardHook();
 
     useEffect(() => {
         setHoleScore({...score});
@@ -145,82 +145,84 @@ function HoleCard(props: Props) {
                     </div>
                     <br/>
                     <div className="HoleForm">
-                        <div className="ScorecardHeader">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                 className="bi bi-x-circle" viewBox="0 0 16 16"
-                                 onClick={() => handlerOnClickAbortRound(scorecard.scorecardId)}>
-                                <path
-                                    d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                <path
-                                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                            <h6>Scorecard</h6>
-                            <div></div>
-                        </div>
-                        <br/>
-                        <div className="form-group">
-                            <label htmlFor={`totalStrokes_${holeNumber}`}>Total Strokes:</label>
-                            <input
-                                type="range"
-                                min="1"
-                                max="14"
-                                className="form-control"
-                                id={`totalStrokes_${holeNumber}`}
-                                name="totalStrokes"
-                                value={holeScore?.totalStrokes || ""}
-                                onChange={handleInputChange}
-                            />
-                            <span>{holeScore?.totalStrokes}</span>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor={`totalPutts_${holeNumber}`}>Total Putts:</label>
-                            <input
-                                type="range"
-                                min="1"
-                                max="7"
-                                className="form-control"
-                                id={`totalPutts_${holeNumber}`}
-                                name="totalPutts"
-                                value={holeScore?.totalPutts || ""}
-                                onChange={handleInputChange}
-                            />
-                            <span>{holeScore?.totalPutts}</span>
-                        </div>
-                        <div className="form-check">
-                            <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id={`fairwayHit_${holeNumber}`}
-                                name="fairwayHit"
-                                checked={holeScore?.fairwayHit || false}
-                                onChange={handleInputChange}
-                            />
-                            <label className="form-check-label" htmlFor={`fairwayHit_${holeNumber}`}>
-                                Fairway Hit
-                            </label>
-                        </div>
-                        <div>
-                            Stableford Net: {stablefordNet}
-                        </div>
-                        <div className="d-flex justify-content-between">
-                            <Button variant="primary" onClick={onPrevHole} disabled={!hasPrevHole}>
-                                Previous Hole
-                            </Button>
-                            {isLastHole ? (
-                                <Button variant="primary" onClick={handleSaveAndScoreCard}>
-                                    Save Scorecard
-                                </Button>
-                            ) : (
-                                <Button variant="primary" onClick={onNextHoleAndSave} disabled={!hasNextHole}>
-                                    Next Hole
-                                </Button>
-                            )}
+                            <div className="AppContent">
+                                <div className="AppHeader">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                         className="bi bi-x-circle" viewBox="0 0 16 16"
+                                         onClick={() => handlerOnClickAbortRound(scorecard.scorecardId)}>
+                                        <path
+                                            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                        <path
+                                            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
+                                    <h6>Scorecard</h6>
+                                    <div></div>
+                                </div>
+                                <br/>
+                                <div className="form-group">
+                                    <label htmlFor={`totalStrokes_${holeNumber}`}>Total Strokes:</label>
+                                    <input
+                                        type="range"
+                                        min="1"
+                                        max="14"
+                                        className="form-control"
+                                        id={`totalStrokes_${holeNumber}`}
+                                        name="totalStrokes"
+                                        value={holeScore?.totalStrokes || ""}
+                                        onChange={handleInputChange}
+                                    />
+                                    <span>{holeScore?.totalStrokes}</span>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor={`totalPutts_${holeNumber}`}>Total Putts:</label>
+                                    <input
+                                        type="range"
+                                        min="1"
+                                        max="7"
+                                        className="form-control"
+                                        id={`totalPutts_${holeNumber}`}
+                                        name="totalPutts"
+                                        value={holeScore?.totalPutts || ""}
+                                        onChange={handleInputChange}
+                                    />
+                                    <span>{holeScore?.totalPutts}</span>
+                                </div>
+                                <div className="form-check">
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input"
+                                        id={`fairwayHit_${holeNumber}`}
+                                        name="fairwayHit"
+                                        checked={holeScore?.fairwayHit || false}
+                                        onChange={handleInputChange}
+                                    />
+                                    <label className="form-check-label" htmlFor={`fairwayHit_${holeNumber}`}>
+                                        Fairway Hit
+                                    </label>
+                                </div>
+                                <div>
+                                    Stableford Net: {stablefordNet}
+                                </div>
+                                <div className="d-flex justify-content-between">
+                                    <Button variant="primary" onClick={onPrevHole} disabled={!hasPrevHole}>
+                                        Previous Hole
+                                    </Button>
+                                    {isLastHole ? (
+                                        <Button variant="primary" onClick={handleSaveAndScoreCard}>
+                                            Save Scorecard
+                                        </Button>
+                                    ) : (
+                                        <Button variant="primary" onClick={onNextHoleAndSave} disabled={!hasNextHole}>
+                                            Next Hole
+                                        </Button>
+                                    )}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
             </Container>
         </div>
-    );
+);
 }
 
 export default HoleCard;
